20902516295b6c320567d23cd029e6eb26d56f80
Pkamal2002 <135655190+Pkamal2002@users.noreply.github.com>
2024-07-23 00:30:06 +0530

    feet/utils codes

--- src/index.js
+++ src/index.js
@@ -7,3 +7,12 @@ configDotenv({ path: './.env' });
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(` Server is running at port: ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.error('Error connecting to the database:', err);
+    process.exit(1);  // Exit the process with an error code
+})
